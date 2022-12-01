75ab33e9a5d7b7e627ecb12fa0aa597bfa5c946f
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2022-12-01 14:20:06 +0700

    koneksi ke database

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$database = "cms";
+$username = "root";
+$password = "";
+$db = mysqli_connect($servername, $username, $password, $database); // membuat koneksi
+// mengecek koneksi
+if (!$db) {
+    die("Koneksi gagal: ".mysqli_connect_error());
+}
+echo "Koneksi berhasil";
+mysqli_close($db);
+?>
