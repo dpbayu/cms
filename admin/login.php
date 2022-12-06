8551aa865e24f9a3e116236ee1b4e4929f9672a7
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2022-12-07 06:31:38 +0700

    rapiin footer

--- admin/login.php
+++ admin/login.php
@@ -117,15 +117,7 @@ require_once "../includes/db.php";
         <!-- Outer Row End -->
     </div>
 
-    <!-- JS Start -->
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-    <!-- JS End -->
+
 
 </body>
 
